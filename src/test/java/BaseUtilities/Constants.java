215e2aa3164f2fd49ae9618076ffaef3e297bdb0
gogunleye <gogunleye@ssl247.co.uk>
2017-12-27 10:22:04 +0000

    Excel File Path Edit

--- src/test/java/BaseUtilities/Constants.java
+++ src/test/java/BaseUtilities/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String URL = "https://ssl247:wpmsBv9P@automation-live.ssl247.co.uk";
     //public static final String File_Path = "C://workspace//AutomationTestScripts//";
     public static final String File_Path = "./";
-    public static final String ExcelFile_Path = "./AutomationTestScripts/";
+    public static final String ExcelFile_Path = "./";
     public static final String ExcelFile_Name = "ExcelData.xlsx";
     public static final String DataFile_Name = "DataDriving.Properties";
   //  public static final String CsvFile_Path = "C:\\ExtentReport\\LinksTest.csv";
